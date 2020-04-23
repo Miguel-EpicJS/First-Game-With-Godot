f83396bcdd5f82071d070a7ab877af0bdb10f95f
Miguel Martinez <51725685+Miguel-EpicJS@users.noreply.github.com>
2020-04-23 08:13:05 -0300

    Update Player.gd

--- First Game/Player.gd
+++ First Game/Player.gd
@@ -1,23 +1,24 @@
-extends KinematicBody2D
+extends KinematicBody2D #impotando a casse KinematicBody2D
 
-var motion = Vector2()
+var motion = Vector2()#Variavel de movimento
 
-func _physics_process(delta):
-	if Input.is_action_pressed("ui_right"):
+func _physics_process(delta):#delta são os FPS
+	if Input.is_action_pressed("ui_right"):#verificação da seta direita
 		
-		motion.x = 100
+		motion.x = 100#movimento
 		move_and_slide(motion)
-	if Input.is_action_pressed("ui_left"):
+	if Input.is_action_pressed("ui_left"):#verificação da seta esquerda
 
-		motion.x = -100
+		motion.x = -100#movimento
 		move_and_slide(motion)
-	if Input.is_action_pressed("ui_down"):
+	if Input.is_action_pressed("ui_down"):#verificação da seta baixo
 
-		motion.y = 100
+		motion.y = 100#movimento
 		move_and_slide(motion)
-	if Input.is_action_pressed("ui_up"):
+	if Input.is_action_pressed("ui_up"):#verificação da seta cima
+	
 		
-		motion.y = -100
+		motion.y = -100#movimento
 		move_and_slide(motion)
 	motion.y = 0
 	motion.x = 0
