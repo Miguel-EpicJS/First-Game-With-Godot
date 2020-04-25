59bdbcd41678829df0ca77f1913652ed18a62b97
Miguel Martinez <51725685+Miguel-EpicJS@users.noreply.github.com>
2020-04-25 08:26:29 -0300

    Add files via upload

---
+++ First Game/Assets/Action RPG Resources/Player/Player.gd
@@ -0,0 +1,33 @@
+extends KinematicBody2D
+
+const ACCELERATION = 500
+const MAX_SPEED = 50
+const FRICTION = 500
+
+var speed = Vector2.ZERO
+
+onready var animationPlayer = $AnimationPlayer
+
+onready var animationTree = $AnimationTree
+
+
+func _physics_process(delta):
+	
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+	
+	
+	if input_vector != Vector2.ZERO:
+		
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Run/blend_position", input_vector)
+		
+		speed = speed.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+		
+	else:
+		
+		speed = speed.move_toward(Vector2.ZERO, FRICTION * delta)
+	
+	speed = move_and_slide(speed)
