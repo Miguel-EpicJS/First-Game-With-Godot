fea61e64392a67bf024a169b67eda913eaac4c14
Miguel Martinez <51725685+Miguel-EpicJS@users.noreply.github.com>
2020-05-01 14:52:00 -0300

    Add files via upload

--- First Game/Assets/Action RPG Resources/Player/Player.gd
+++ First Game/Assets/Action RPG Resources/Player/Player.gd
@@ -4,7 +4,15 @@ const ACCELERATION = 500
 const MAX_SPEED = 50
 const FRICTION = 500
 
+enum{
+	MOVE,
+	ROLL,
+	ATTACK
+}
+
 var speed = Vector2.ZERO
+
+var state = MOVE
 
 onready var animationPlayer = $AnimationPlayer
 
@@ -12,7 +20,20 @@ onready var animationTree = $AnimationTree
 
 onready var animationState = animationTree.get("parameters/playback")
 
+func _ready():
+	animationTree.active = true
+
 func _physics_process(delta):
+	match state:
+		MOVE:
+			move_state(delta)
+		ROLL:
+			pass
+		ATTACK:
+			attack_state(delta)
+
+func move_state(delta):
+		
 	
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
@@ -24,6 +45,7 @@ func _physics_process(delta):
 		
 		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationTree.set("parameters/Attack/blend_position", input_vector)
 		animationState.travel("Run")
 		speed = speed.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 		
@@ -32,3 +54,12 @@ func _physics_process(delta):
 		speed = speed.move_toward(Vector2.ZERO, FRICTION * delta)
 	
 	speed = move_and_slide(speed)
+	
+	if Input.is_action_just_pressed("attack"):
+		state = ATTACK
+
+func attack_state(delta):
+	animationState.travel("Attack")
+
+func attack_animation_finished():
+	state = MOVE
